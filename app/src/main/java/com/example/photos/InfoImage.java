8cfab42ab9e46b295071e9ba5511d5240f4252f4
unknown <huyhaile13>
2022-03-17 18:04:03 +0700

    first commit

---
+++ app/src/main/java/com/example/photos/InfoImage.java
@@ -0,0 +1,34 @@
+package com.example.photos;
+
+import android.content.Intent;
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.squareup.picasso.Picasso;
+
+public class InfoImage extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gridview_item);
+
+        Intent intent = getIntent();
+
+        ImageView imageView = findViewById(R.id.myImage);
+
+        int posMenu = intent.getIntExtra("PosMenu", 0);
+        int posItem = intent.getIntExtra("PosItem", 0);
+
+
+        TypedArray images = getResources().obtainTypedArray(R.array.images);
+        int contentId = images.getResourceId(posMenu, R.array.flowers);
+        String imageURL = getResources().getStringArray(contentId)[posItem];
+        images.recycle();
+
+        Picasso.get().load(imageURL).resize(500, 500).into(imageView);
+    }
+}
---
+++ app/src/main/java/com/example/photos/MenuFragment.java
@@ -0,0 +1,32 @@
+package com.example.photos;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MenuFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        String[] menu = getResources().getStringArray(R.array.menu);
+
+        ListView listView = view.findViewById(R.id.myListView);
+        listView.setAdapter(new ArrayAdapter<>(getActivity(), R.layout.listview_item, menu));
+        listView.setOnItemClickListener((adapterView, view1, i, l) -> {
+            ContentFragment contentFragment = (ContentFragment) getParentFragmentManager().findFragmentById(R.id.contentFragment);
+            contentFragment.updateFrag(i);
+        });
+
+        return  view;
+    }
+}
